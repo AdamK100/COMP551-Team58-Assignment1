68f8752368c592a0d6f07a220ab787ba15afc3b7
philippesh <sarouphim.philippe@gmail.com>
2022-09-23 20:40:27 -0400

    Remove evaluate_acc from main.py

--- main.py
+++ main.py
@@ -6,6 +6,3 @@ import inputs
 knn = models.KNN_Graph()
 
 knn.fit(1, inputs.hepatitis_clean_data, inputs.hepatitis_clean_data[0,:])
-
-def evaluate_acc(true_labels: list, target_labels: list):
-    return
