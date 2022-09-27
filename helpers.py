30f82ea7ef7e0a1207aebe046aec7846a82b0f36
philippesh <sarouphim.philippe@gmail.com>
2022-09-27 16:27:34 -0400

    Simplify helper methods to use numpy array methods

--- helpers.py
+++ helpers.py
@@ -1,40 +1,17 @@
 import numpy as np
 
 def euclidean_distance(x1: list[float], x2: list[float]) -> float:
-    if len(x1) != len(x2): return -1
-
-    dist_squared: float = 0
-    for i in range(len(x1)):
-        dist_squared += (x1[i] - x2[i]) ** 2
-    
-    return np.sqrt(dist_squared)
+    return np.sqrt(np.sum((np.array(x2) - np.array(x1)) ** 2))
 
 
 def most_common_label(l: list):
-    map: dict = {}
-    for item in l:
-        if item in map.keys():
-            map[item] = map[item] + 1
-        else:
-            map[item] = 1
-            
-
-    max_label = list(map.keys())[0]
-    max: int = map[max_label]
-    for key in map:
-        if map[key] > max:
-            max_label = key
-            max = map[max_label]
-    
-    return max_label
+    return np.bincount(np.array(l, int)).argmax()
 
 
 def evaluate_acc(true_labels: list, target_labels: list) -> float:
     return np.sum(true_labels == target_labels) / len(true_labels)
 
-def gini_index(lbls):
-    cprobs = np.bincount(lbls.astype(int)) / len(lbls)
-    gini = 0
-    for p in cprobs:
-        gini += (p * (1-p))
-    return gini
+
+def gini_index(labels):
+    class_probs = np.bincount(labels.astype(int)) / len(labels)
+    return 1 - np.sum(class_probs ** 2)
