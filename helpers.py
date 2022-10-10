1a62ab1372413c65e7514a4d0c644bc23b18009a
philippesh <sarouphim.philippe@gmail.com>
2022-10-10 01:48:17 -0400

    Add manhattan distance function

--- helpers.py
+++ helpers.py
@@ -5,6 +5,10 @@ def euclidean_distance(x1: list[float], x2: list[float]) -> float:
     return np.sqrt(np.sum((np.array(x2) - np.array(x1)) ** 2))
 
 
+def manhattan_distance(x1: list[float], x2: list[float]) -> float:
+    return np.sum(np.absolute(np.array(x1) - np.array(x2)))
+
+
 def most_common_label(l: list):
     return np.bincount(np.array(l, int)).argmax()
 
@@ -17,8 +21,10 @@ def gini_index(labels):
     class_probs = np.bincount(labels.astype(int)) / len(labels)
     return 1 - np.sum(class_probs ** 2)
 
+
 def cosine_similarity(vector1,vector2):
     return np.sum(vector1 * vector2)/(sqrt(np.sum(vector1 ** 2)) * sqrt(np.sum(vector2 ** 2)))
+
 
 def remove_irrelevant_features(data : np.ndarray, true_labels : np.ndarray, nb_features : int) -> np.ndarray:
     # similarities = np.apply_along_axis(lambda d: cosine_similarity(d, true_labels), 0, data)
