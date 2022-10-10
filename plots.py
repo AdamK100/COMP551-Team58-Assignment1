058cef202af9a22c7150690660cd688b4e3a137f
philippesh <sarouphim.philippe@gmail.com>
2022-10-10 01:48:53 -0400

    Add support for multi-data-set plots

--- plots.py
+++ plots.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot(hyperparameters: np.ndarray, accuracies: np.ndarray, hyperparameter_name: str):
-    plt.plot(hyperparameters, accuracies * 100)
-    plt.xlabel(hyperparameter_name)
+colors = ['b', 'r', 'g']
+
+def plot(x_data: np.ndarray, y_datas: list[np.ndarray], x_name: str, y_names: list[str]):
+    figure = plt.figure()
+    axes = figure.add_subplot(111)
+    
+    for i in range(len(y_datas)):
+        axes.plot(x_data, y_datas[i] * 100, label = y_names[i], c=colors[i % 3])
+
+    plt.xlabel(x_name)
     plt.ylabel("Accuracy")
-    plt.axis([hyperparameters[0], hyperparameters[-1], 0, 100])
+    plt.axis([x_data[0], x_data[-1], 0, 100])
+    
+    plt.legend(loc='lower right')
     plt.show()
